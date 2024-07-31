a2bd272fc3fecae9a50d0f94acdf77bdb35c69f8
bleuxsy <100823803+bleuxsy@users.noreply.github.com>
2024-07-31 19:52:00 +0900

    Title: 사칙연산 함수, Time: 122ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240731/사칙연산 함수/quadratic-operations-function.py
@@ -0,0 +1,25 @@
+def summ(x,y):
+    return (x+y)
+def minus(x,y):
+    return (x-y)
+def mul(x,y):
+    return(x*y)
+def remind(x,y):
+    return(x/y)
+
+result = 0
+a, o, c= input().split()
+if o =="+":
+    result = summ(int(a),int(c))
+    print(a,o,c,"=",result,end=" ")
+elif o =="-":
+   result=  minus(int(a),int(c))
+   print(a,o,c,"=",result,end=" ")
+elif o =="*":
+    result = mul(int(a),int(c))
+    print(a,o,c,"=",result,end=" ")
+elif o =="/":
+    result = remind(int(a),int(c))
+    print(a,o,c,"=",result,end=" ")
+else:
+    print("false")
