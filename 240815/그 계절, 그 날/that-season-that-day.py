1c76279cf9ca3f73ccaae766d53e54b8f551a5cc
bleuxsy <100823803+bleuxsy@users.noreply.github.com>
2024-08-15 20:22:42 +0900

    Title: 그 계절, 그 날, Time: 103ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240815/그 계절, 그 날/that-season-that-day.py
@@ -0,0 +1,37 @@
+def notyoon(y):
+    if y%4 ==0 :
+        if y%100 == 0:
+            if y%400 == 0:
+                return False
+            return True
+        return False
+    return True
+def isMonth(y,m,d):
+    if m in [1,3,5,7,8,10,12]:
+        if d <32:
+            return True
+        return False
+    if m in [2,4,6,9,11]:
+        if d<31 : 
+            return True
+        return False
+    if m == 2:
+        if notyoon(y) and d < 29: 
+            return True
+        if notyoon(y):
+            if d  > 29 : 
+                return False
+        return True
+
+Y, M, D = map(int, input().split())
+if isMonth(Y,M,D):
+    if M in [3,4,5]:
+        print("Spring")
+    elif M in [6,7,8]:
+        print("Summer")
+    elif M in [9,10,11]:
+        print("Fall")
+    else :
+        print("Winter")
+else:
+    print(-1)
