1117e281d8264a8a737fdf34d7d04758ef2da1e8
bleuxsy <100823803+bleuxsy@users.noreply.github.com>
2024-08-18 22:07:03 +0900

    Title: 함수를 이용한 부분 문자열의 위치 구하기, Time: 127ms, Memory: 24MB, Status: Passed - CodeTree

--- 240818/함수를 이용한 부분 문자열의 위치 구하기/find-the-location-of-a-substring-using-a-function.py
+++ 240818/함수를 이용한 부분 문자열의 위치 구하기/find-the-location-of-a-substring-using-a-function.py
@@ -1,4 +1,9 @@
-A = input()
-B = input()
-for word in A : 
-    if word in B :
+# 입력 문자열과 목적 문자열을 받습니다.
+input_string = input().strip()
+target_string = input().strip()
+
+# 목적 문자열이 입력 문자열의 부분 문자열로 존재하는 경우 그 시작 인덱스를 출력
+index = input_string.find(target_string)
+
+# 인덱스를 출력합니다. (존재하지 않을 경우 -1이 출력됩니다.)
+print(index)
