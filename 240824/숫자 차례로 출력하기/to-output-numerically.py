39afa89ac009769fbef06d59fd1ab9257585daf3
bleuxsy <100823803+bleuxsy@users.noreply.github.com>
2024-08-24 23:26:38 +0900

    Title: 숫자 차례로 출력하기, Time: 111ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240824/숫자 차례로 출력하기/to-output-numerically.py
@@ -0,0 +1,20 @@
+def print_ascending(N):
+    if N == 0:  # 재귀 종료 조건
+        return
+    print_ascending(N - 1)  # N-1까지 출력
+    print(N)  # 현재 N 출력
+
+def print_descending(N):
+    if N == 0:  # 재귀 종료 조건
+        return
+    print(N)  # 현재 N 출력
+    print_descending(N - 1)  # N-1까지 출력
+
+# N 입력 받기
+N = int(input())
+
+# 1부터 N까지 출력
+print_ascending(N)
+
+# N부터 1까지 출력
+print_descending(N)
