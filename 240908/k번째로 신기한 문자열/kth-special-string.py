d1ba650ee3e5c217697a73c39ddcefce81852085
bleuxsy <100823803+bleuxsy@users.noreply.github.com>
2024-09-08 22:59:06 +0900

    Title: k번째로 신기한 문자열, Time: 107ms, Memory: 24MB, Status: Passed - CodeTree

--- 240908/k번째로 신기한 문자열/kth-special-string.py
+++ 240908/k번째로 신기한 문자열/kth-special-string.py
@@ -1,9 +1,18 @@
-N, K,T = input().split()
+# 입력 받기
+N, K, T = input().split()
 
-mylist =[]
+# 단어를 저장할 리스트
+mylist = []
+
+# N개의 단어 입력 받기
 for i in range(int(N)):
     word = input()
-    if T in word:
+    # 단어가 T로 시작하는 경우 리스트에 추가
+    if word.startswith(T):
         mylist.append(word)
-mylist= sorted(mylist)
+
+# 사전 순으로 정렬
+mylist = sorted(mylist)
+
+# K번째 단어 출력 (1-based index 이므로 K-1 사용)
 print(mylist[int(K)-1])
