d5f011cd9fe6aa0cd53dd6907d19827c08d44a22
bleuxsy <100823803+bleuxsy@users.noreply.github.com>
2024-10-01 22:23:08 +0900

    Title: 중앙값 계산 2, Time: 221ms, Memory: 24MB, Status: Passed - CodeTree

--- 241001/중앙값 계산 2/get-median-2.py
+++ 241001/중앙값 계산 2/get-median-2.py
@@ -1,12 +1,13 @@
 n = int(input())
 mylist= list(map(int,input().split()))
-mylist.sort()
 i= 0 
+sublist=[]
 while(n>i):
     if i == 0 :
         print(mylist[0],end=' ')
         i+=1
         continue;
     elif i%2 == 0:
-        print(mylist[i//2],end=' ')
+        sublist= sorted(mylist[:i+1])
+        print(sublist[i//2],end=' ')
     i+=1
