fed88e01d067b110dee81770144db13bd176e9ed
Seungyeon Kim <sungyeon1227@gmail.com>
2026-02-13 21:38:29 +0900

    Title: 강력한 폭발, Time: 61ms, Memory: 17MB, Status: Passed - Codetree

--- 260213/강력한 폭발/strong-explosion.py
+++ 260213/강력한 폭발/strong-explosion.py
@@ -1,9 +1,6 @@
-import copy
-
 n = int(input())
 grid = [list(map(int, input().split())) for _ in range(n)]
 
-# 폭탄 위치 저장
 bombs = []
 for i in range(n):
     for j in range(n):
@@ -13,59 +10,58 @@ for i in range(n):
 max_area = 0
 
 
-def first(board, x, y):
-    for i in range(x - 2, x + 3):
-        if 0 <= i < n:
-            board[i][y] = 1
+def explode(x, y, bomb_type):
+    changed = []
+
+    if bomb_type == 0:  # first (세로 5칸)
+        for i in range(x - 2, x + 3):
+            if 0 <= i < n and grid[i][y] == 0:
+                grid[i][y] = 1
+                changed.append((i, y))
+
+    elif bomb_type == 1:  # second (상하좌우)
+        dx = [0, 1, 0, -1]
+        dy = [1, 0, -1, 0]
+        for d in range(4):
+            nx = x + dx[d]
+            ny = y + dy[d]
+            if 0 <= nx < n and 0 <= ny < n and grid[nx][ny] == 0:
+                grid[nx][ny] = 1
+                changed.append((nx, ny))
+
+    else:  # third (대각선)
+        dx = [1, 1, -1, -1]
+        dy = [1, -1, -1, 1]
+        for d in range(4):
+            nx = x + dx[d]
+            ny = y + dy[d]
+            if 0 <= nx < n and 0 <= ny < n and grid[nx][ny] == 0:
+                grid[nx][ny] = 1
+                changed.append((nx, ny))
+
+    return changed
 
 
-def second(board, x, y):
-    board[x][y] = 1
-    dx = [0, 1, 0, -1]
-    dy = [1, 0, -1, 0]
-    for d in range(4):
-        nx = x + dx[d]
-        ny = y + dy[d]
-        if 0 <= nx < n and 0 <= ny < n:
-            board[nx][ny] = 1
+def restore(changed):
+    for x, y in changed:
+        grid[x][y] = 0
 
 
-def third(board, x, y):
-    board[x][y] = 1
-    dx = [1, 1, -1, -1]
-    dy = [1, -1, -1, 1]
-    for d in range(4):
-        nx = x + dx[d]
-        ny = y + dy[d]
-        if 0 <= nx < n and 0 <= ny < n:
-            board[nx][ny] = 1
-
-
-def dfs(idx, board):
+def dfs(idx):
     global max_area
 
     if idx == len(bombs):
-        # 1의 개수 세기
-        count = sum(sum(row) for row in board)
+        count = sum(sum(row) for row in grid)
         max_area = max(max_area, count)
         return
 
     x, y = bombs[idx]
 
-    # 3가지 선택지
     for bomb_type in range(3):
-        new_board = copy.deepcopy(board)
-
-        if bomb_type == 0:
-            first(new_board, x, y)
-        elif bomb_type == 1:
-            second(new_board, x, y)
-        else:
-            third(new_board, x, y)
-
-        dfs(idx + 1, new_board)
+        changed = explode(x, y, bomb_type)
+        dfs(idx + 1)
+        restore(changed)
 
 
-dfs(0, grid)
-
+dfs(0)
 print(max_area)
