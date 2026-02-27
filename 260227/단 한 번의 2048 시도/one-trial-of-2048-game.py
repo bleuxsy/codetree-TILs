f4bdae60ede2d12e6aaf6ceca8b5976387f96458
Seungyeon Kim <sungyeon1227@gmail.com>
2026-02-27 15:01:13 +0900

    Title: 단 한 번의 2048 시도, Time: 50ms, Memory: 16MB, Status: Passed - Codetree

--- 260227/단 한 번의 2048 시도/one-trial-of-2048-game.py
+++ 260227/단 한 번의 2048 시도/one-trial-of-2048-game.py
@@ -8,13 +8,13 @@ dir = input()
 def left(lst):
     newgrid = [[0]*4 for _ in range(4)]
     for i in range(4):
-        # 1) 왼 -> 오로 0 아닌 값 모으기
+     
         vals = []
         for j in range(4):
             if lst[i][j] != 0:
                 vals.append(lst[i][j])
 
-        # 2) 왼쪽부터 합치기
+      
         temp = deque()
         k = 0
         while k < len(vals):
@@ -25,11 +25,10 @@ def left(lst):
                 temp.append(vals[k])
                 k += 1
 
-        # 3) 오른쪽에 0 채워서 길이 4 맞추기
         while len(temp) < 4:
             temp.append(0)
 
-        # 4) 행에 왼쪽부터 채우기
+     
         for j in range(4):
             newgrid[i][j] = temp[j]
 
@@ -39,13 +38,13 @@ def left(lst):
 def right(lst):
     newgrid = [[0]*4 for _ in range(4)]
     for i in range(4):
-        # 1) 오 -> 왼으로 0 아닌 값 모으기 (right-first)
+        
         vals = []
         for j in range(3, -1, -1):
             if lst[i][j] != 0:
                 vals.append(lst[i][j])
 
-        # 2) 오른쪽부터 합치기
+        
         temp = deque()
         k = 0
         while k < len(vals):
@@ -56,7 +55,7 @@ def right(lst):
                 temp.append(vals[k])
                 k += 1
 
-        # 3) 왼쪽이 0이 되도록 패딩 (temp는 right->left 순서)
+      
         while len(temp) < 4:
             temp.append(0)
 
@@ -70,13 +69,13 @@ def right(lst):
 def up(lst):
     newgrid = [[0]*4 for _ in range(4)]
     for j in range(4):
-        # 1) 위 -> 아래로 0 아닌 값 모으기
+        
         vals = []
         for i in range(4):
             if lst[i][j] != 0:
                 vals.append(lst[i][j])
 
-        # 2) 위쪽부터 합치기
+       
         temp = deque()
         k = 0
         while k < len(vals):
@@ -87,11 +86,11 @@ def up(lst):
                 temp.append(vals[k])
                 k += 1
 
-        # 3) 아래쪽에 0 채워서 길이 4 맞추기
+       
         while len(temp) < 4:
             temp.append(0)
 
-        # 4) 열에 위부터 채우기
+     
         for i in range(4):
             newgrid[i][j] = temp[i]
 
@@ -118,13 +117,13 @@ def down(lst):
                 temp.append(vals[k])
                 k += 1
 
-        # 3) 위쪽이 0이 되도록 패딩 (temp는 bottom->top)
+       
         while len(temp) < 4:
             temp.append(0)
 
-        # 4) 열에 아래부터 채우기
+     
         for i in range(3, -1, -1):
-            newgrid[i][j] = temp[3 - i]  # i=3(bottom) <- temp[0]
+            newgrid[i][j] = temp[3 - i]  
 
     for r in newgrid:
         print(*r)
