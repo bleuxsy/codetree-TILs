4f0a88b94c483167cf4a8a4400ea2c61614564b7
Seungyeon Kim <sungyeon1227@gmail.com>
2026-02-27 17:56:12 +0900

    Title: 벽 짚고 미로 탈출하기, Time: 53ms, Memory: 16MB, Status: Wrong Answer - Codetree

--- 260227/벽 짚고 미로 탈출하기/escape-maze-with-wall-following.py
+++ 260227/벽 짚고 미로 탈출하기/escape-maze-with-wall-following.py
@@ -4,46 +4,53 @@ x, y = map(int, input().split())
 
 grid = [["."] * (N + 1) for _ in range(N + 1)]
 for i in range(1, N + 1):
-    row = input()
+    row = input().strip()
     for j in range(1, N + 1):
         grid[i][j] = row[j - 1]
 
-# Please write your code here.
-
 dx = [0, -1, 0, 1]
 dy = [1, 0, -1, 0]
+
 sd = 0
 stack = deque()
-stack.append((x,y, sd))
+stack.append((x, y, sd))
 time = 1
-visited = list([0]*(N+1) for _ in range(N+1))
+
+visited = [[0] * (N + 1) for _ in range(N + 1)]
 visited[x][y] = 1
+
 while stack:
-    sx , sy , sd = stack.popleft()
-    if 1<= sx+dx[sd-1] <= N and 1<= sy+dy[sd-1] <= N:
-        if grid[sx+dx[sd-1]][sy+dy[sd-1]] == ".":
-            sd = sd-1
-    #진행 방향의 다음 칸
+    sx, sy, sd = stack.popleft()
+
     nx = sx + dx[sd]
     ny = sy + dy[sd]
-    if 1<= nx <= N and 1<= ny <= N :
-    #다음 칸에 벽이 없을 때,
+
+    if 1 <= nx <= N and 1 <= ny <= N:
         if visited[nx][ny] == 0:
             if grid[nx][ny] == ".":
-                #오른쪽에 벽이 없을 때,
-                nd = sd - 1
-                stack.append((nx, ny, nd))
-                time += 1
-            else:
-                nd = sd +1
-                stack.append((nx, ny, nd))
-                time += 1
                 
+                nd = (sd + 3) % 4
+
+                rx = nx + dx[nd]
+                ry = ny + dy[nd]
+
+                
+                if 1 <= rx <= N and 1 <= ry <= N and grid[rx][ry] == ".":
+                    visited[nx][ny] = 1
+                    visited[rx][ry] = 1
+                    stack.append((rx, ry, nd))
+                    time += 2
+                else:
+                    visited[nx][ny] = 1
+                    stack.append((nx, ny, sd))
+                    time += 1
+            else:
+                # 벽이면 방향만 바꿈
+                nd = (sd + 1) % 4
+                stack.append((sx, sy, nd))
         else:
             print(-1)
             break
-    # 미로 끝
     else:
         print(time)
         break
-
