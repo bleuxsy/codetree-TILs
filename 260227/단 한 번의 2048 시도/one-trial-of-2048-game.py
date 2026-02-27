b9c472a401d5dea22a5315a0fc819ebf846f33bc
Seungyeon Kim <sungyeon1227@gmail.com>
2026-02-27 14:39:46 +0900

    Title: 단 한 번의 2048 시도, Time: 52ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 260227/단 한 번의 2048 시도/one-trial-of-2048-game.py
@@ -0,0 +1,72 @@
+# Read 4x4 grid
+grid = [list(map(int, input().split())) for _ in range(4)]
+from collections import deque
+# Read direction
+dir = input()
+def left(lst):
+    for l in lst:
+        temp = deque()
+        i = 0
+        l.append(0)
+        while i < len(l)-1 :
+            if l[i] == 0:
+                i += 1
+                continue
+            if l[i] == l[i+1]:
+                temp.append(2*l[i])
+                i += 2
+            else:
+                temp.append(l[i])
+                i += 1
+        while len(temp) < 4:
+            temp.append(0)
+        print(*temp)
+def right(lst):
+    for l in lst:
+        temp = deque()
+        i = 3
+        
+        while i > -1  :
+            if l[i] == 0:
+                i -= 1
+                continue
+            if i == 0:
+                temp.appendleft(l[i])
+                break
+            if l[i] == l[i-1]:
+                temp.appendleft(2*l[i])
+                i -= 2
+            else:
+                temp.appendleft(l[i])
+                i -= 1
+        while len(temp) < 4:
+            temp.appendleft(0)
+        print(*temp)
+def down(lst):
+    for j in range(4):
+        for i in range(4):
+            temp = deque()
+        
+            
+            while i < 3 :
+                if lst[i][j] == 0:
+                    i += 1
+                    continue
+                if lst[i][j]== lst[i+1][j]:
+                    temp.append(2*lst[i][j])
+                    i += 2
+                else:
+                    temp.append(lst[i][j])
+                    i += 1
+            while len(temp) < 4:
+                temp.append(0)
+            for r in temp:
+                print(r)
+if dir == 'L':
+    left(grid)
+
+elif dir == 'R':
+    right(grid)
+    #down(grid)
+elif dir == 'D':
+    down(grid)
