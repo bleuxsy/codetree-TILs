8a52e7d544695666dcc87b5f502e14cfab1954d7
Seungyeon Kim <sungyeon1227@gmail.com>
2026-03-09 17:59:32 +0900

    Title: 단순한 동전 챙기기, Time: 45ms, Memory: 16MB, Status: Wrong Answer - Codetree

--- 260309/단순한 동전 챙기기/collect-coins-easy.py
+++ 260309/단순한 동전 챙기기/collect-coins-easy.py
@@ -1,61 +1,39 @@
-INF = 10**18
-n = int(input())
-grid = [list(input()) for _ in range(n)]
+def move(sx, sy, L, num):
+    global answer, best
 
-sx = sy = -1
-total = 0
-
-for i in range(n):
-    for j in range(n):
-        if grid[i][j] == 'S':
-            sx, sy = i, j
-        elif grid[i][j] not in ['S', 'E', '.']:
-            total += 1
-
-if total < 3:
-    print(-1)
-    exit()
-
-answer = INF
-best = {}
-
-def backtrack(x, y, last_num, cnt, dist):
-    global answer
-
-    # 이미 정답보다 길면 중단
-    if dist >= answer:
+    if L >= answer:
         return
 
-   
-    state = (x, y, last_num, cnt)
-    if state in best and best[state] <= dist:
+    last_num = -1 if not num else num[-1]
+    state = (sx, sy, last_num, len(num))
+    if state in best and best[state] <= L:
         return
-    best[state] = dist
+    best[state] = L
 
-    
-    if grid[x][y] == 'E':
-        if cnt >= 3:
-            answer = min(answer, dist)
+    if grid[sx][sy] == 'E':
+        if len(num) >= 3:
+            answer = min(answer, L)
         return
 
-    for dx, dy in [(1,0), (-1,0), (0,1), (0,-1)]:
-        nx, ny = x + dx, y + dy
+    for dx, dy in [(1, 0), (-1, 0), (0, 1), (0, -1)]:
+        nx = sx + dx
+        ny = sy + dy
 
-        if not (0 <= nx < n and 0 <= ny < n):
-            continue
+        if 0 <= nx < n and 0 <= ny < n:
+            cell = grid[nx][ny]
 
-        cell = grid[nx][ny]
+          
+            if cell not in ['.', 'E', 'S']:
+                val = int(cell)
 
-        
-        if cell not in ['S', 'E', '.']:
-            num = int(cell)
-            if num > last_num:
-                backtrack(nx, ny, num, cnt + 1, dist + 1)
+                
+                move(nx, ny, L + 1, num)
 
-        
-        else:
-            backtrack(nx, ny, last_num, cnt, dist + 1)
+               
+                if not num:
+                    move(nx, ny, L + 1, [val])
+                elif val > num[-1]:
+                    move(nx, ny, L + 1, num + [val])
 
-backtrack(sx, sy, -1, 0, 0)
-
-print(-1 if answer == INF else answer)
+            else:
+                move(nx, ny, L + 1, num)
