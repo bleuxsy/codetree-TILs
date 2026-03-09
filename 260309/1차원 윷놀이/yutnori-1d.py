ed9820e4fbb493150139275a8af2f451540f2537
Seungyeon Kim <sungyeon1227@gmail.com>
2026-03-09 15:50:18 +0900

    Title: 1차원 윷놀이, Time: 2923ms, Memory: 25MB, Status: Passed - Codetree

--- 260309/1차원 윷놀이/yutnori-1d.py
+++ 260309/1차원 윷놀이/yutnori-1d.py
@@ -1,32 +1,41 @@
-N, M, K = map(int, input().split())
+# 변수 선언 및 입력:
+n, m, k = tuple(map(int, input().split()))
 nums = list(map(int, input().split()))
+pieces = [1 for _ in range(k)]
+
+ans = 0
 
 
-pos = [1] * (K)
+# 점수를 계산합니다.
+def calc():
+    score = 0
+    for piece in pieces:
+        score += (piece >= m)
+    
+    return score
 
-answer = 0
 
-def back(turn, score):
-    global answer
+def find_max(cnt):
+    global ans
+    
+    # 말을 직접 n번 움직이지 않아도
+    # 최대가 될 수 있으므로 항상 답을 갱신합니다.
+    ans = max(ans, calc())
+    
+    # 더 이상 움직일 수 없으면 종료합니다.
+    if cnt == n: 
+        return
+	
+    for i in range(k):
+        # 움직여서 더 이득이 되지 않는
+        # 말은 더 이상 움직이지 않습니다.
+        if pieces[i] >= m:
+            continue
+        
+        pieces[i] += nums[cnt]
+        find_max(cnt + 1)
+        pieces[i] -= nums[cnt]
 
-    if turn == N:
-        answer = max(answer, score)
-        return 
 
-    move = nums[turn]
-    for i in range(K):
-        old = pos[i]
-        if old == M : 
-            back(turn+1 , score)
-        new = old + move
-        flag = 0
-
-        if new >= M:
-            new = M
-            flag = 1
-        pos[i] = new
-        back(turn +1, score + flag)
-        pos[i] = old
-
-back(1,0)
-print(answer)
+find_max(0)
+print(ans)
