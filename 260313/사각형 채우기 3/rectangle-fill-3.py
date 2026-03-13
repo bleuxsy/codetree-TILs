84832c7ab245aebcaa243e3676dd468e36f1d4ab
Seungyeon Kim <sungyeon1227@gmail.com>
2026-03-13 16:07:41 +0900

    Title: 사각형 채우기 3, Time: 45ms, Memory: 16MB, Status: Passed - Codetree

--- 260313/사각형 채우기 3/rectangle-fill-3.py
+++ 260313/사각형 채우기 3/rectangle-fill-3.py
@@ -1,13 +1,13 @@
+MOD = 1000000007
+
 n = int(input())
 
-dp = [0] * (n + 1)
-
-if n >= 1:
-    dp[1] = 2
-if n >= 2:
-    dp[2] = 7
+dp = [0] * (max(3, n + 1))
+dp[0] = 1
+dp[1] = 2
+dp[2] = 7
 
 for i in range(3, n + 1):
-    dp[i] = 3* dp[i - 1] +1
+    dp[i] = (3 * dp[i - 1] + dp[i - 2] - dp[i - 3]) % MOD
 
-print(dp[n]%1000000007)
+print(dp[n])
