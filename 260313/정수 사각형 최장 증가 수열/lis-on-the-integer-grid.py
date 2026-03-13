144d7a4e5e62662997a7abd79bdf98973b1396cf
Seungyeon Kim <sungyeon1227@gmail.com>
2026-03-13 17:31:50 +0900

    Title: 정수 사각형 최장 증가 수열, Time: 546ms, Memory: 57MB, Status: Passed - Codetree

--- 260313/정수 사각형 최장 증가 수열/lis-on-the-integer-grid.py
+++ 260313/정수 사각형 최장 증가 수열/lis-on-the-integer-grid.py
@@ -1,28 +1,27 @@
 n = int(input())
-grid = [list(map(int,input().split())) for _ in range(n)]
+grid = [list(map(int, input().split())) for _ in range(n)]
 
-dp = [[0]*n for _ in range(n)]
+dp = [[1] * n for _ in range(n)]
 
-def dfs(x,y):
-    if dp[x][y] != 0:
-        return dp[x][y]
-
-    dp[x][y] = 1
-
-    for dx,dy in [(-1,0),(1,0),(0,1),(0,-1)]:
-        nx,ny = x+dx,y+dy
-
-        if 0<=nx<n and 0<=ny<n:
-            if grid[nx][ny] > grid[x][y]:
-                dp[x][y] = max(dp[x][y], dfs(nx,ny)+1)
-
-    return dp[x][y]
-
-
-answer = 0
-
+cells = []
 for i in range(n):
     for j in range(n):
-        answer = max(answer, dfs(i,j))
+        cells.append((grid[i][j], i, j))
+
+cells.sort()  # 작은 값부터
+
+dirs = [(-1, 0), (1, 0), (0, -1), (0, 1)]
+
+for value, x, y in cells:
+    for dx, dy in dirs:
+        nx, ny = x + dx, y + dy
+        if 0 <= nx < n and 0 <= ny < n:
+            if grid[nx][ny] > grid[x][y]:
+                dp[nx][ny] = max(dp[nx][ny], dp[x][y] + 1)
+
+answer = 0
+for i in range(n):
+    for j in range(n):
+        answer = max(answer, dp[i][j])
 
 print(answer)
