2f13df645b05d2100e24e6817e73a0357afa4ef4
Seungyeon Kim <sungyeon1227@gmail.com>
2026-03-13 15:35:31 +0900

    Title: 피보나치 수, Time: 49ms, Memory: 16MB, Status: Passed - Codetree

--- 260313/피보나치 수/fibonacci-number.py
+++ 260313/피보나치 수/fibonacci-number.py
@@ -1,7 +1,12 @@
-N = int(input())
-def fib(n):
-    if n == 1 or n == 2:
-        return 1
-    return fib(n-1) + fib(n-2)
+n = int(input())
 
-print(fib(N))
+MAX_NUM = 45
+
+dp = [0 for _ in range(MAX_NUM + 1)]
+
+dp[1] = dp[2] = 1
+
+for i in range(3, n + 1):
+    dp[i] = dp[i - 1] + dp[i - 2]
+    
+print(dp[n])
