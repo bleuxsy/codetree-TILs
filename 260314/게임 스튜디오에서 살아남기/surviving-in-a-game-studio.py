fdeece1faca2032e40f3a9449afee4e1d27de35a
Seungyeon Kim <sungyeon1227@gmail.com>
2026-03-14 18:40:19 +0900

    Title: 게임 스튜디오에서 살아남기, Time: 53ms, Memory: 16MB, Status: Wrong Answer - Codetree

--- 260314/게임 스튜디오에서 살아남기/surviving-in-a-game-studio.py
+++ 260314/게임 스튜디오에서 살아남기/surviving-in-a-game-studio.py
@@ -12,16 +12,20 @@ dp[0][2] = 3
 dp[1][2] = 3
 dp[2][2] = 3
 
-dp[0][3] = 9
-dp[1][3] = 9
-dp[2][3] = 8
-for i in range(4,n+1):
-    # Good
-    dp[0][i] = dp[0][i-1] + dp[1][i-1] + dp[2][i-1]
-    # Bad
-    dp[1][i] = dp[0][i] - 2 ** (i-3)
-    # Te
-    dp[2][i] = dp[0][i] - ((i-1)*(i-2)* 2**(i-4))
+if n >= 3:
+    dp[0][3] = 9
+    dp[1][3] = 8
+    dp[2][3] = 8
+
+    for i in range(4,n+1):
+        
+        # Good
+        dp[0][i] = dp[0][i-1] + dp[1][i-1] + dp[2][i-1]
+        # Bad
+        dp[1][i] = dp[0][i] - 2 ** (i-3)
+        # Te
+        dp[2][i] = dp[0][i] - ((i-1)*(i-2)* 2**(i-4))
+
 
 answer = int(dp[0][n] + dp[1][n] + dp[2][n])
 print(answer%MAX)
