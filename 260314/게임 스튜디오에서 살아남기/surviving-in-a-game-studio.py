3f380c3f7fb8a730cb478cf22ddefe31605acf28
Seungyeon Kim <sungyeon1227@gmail.com>
2026-03-14 18:37:59 +0900

    Title: 게임 스튜디오에서 살아남기, Time: 75ms, Memory: 19MB, Status: Runtime Error - Codetree

--- 260314/게임 스튜디오에서 살아남기/surviving-in-a-game-studio.py
+++ 260314/게임 스튜디오에서 살아남기/surviving-in-a-game-studio.py
@@ -1,30 +1,27 @@
 n = int(input())
 MAX = 10**9 + 7
 
-dp = [[0] * (n + 1) for _ in range(3)]
+# Please write your code here.
+dp = [[0] *(n+1) for _ in range(3)]
 
-if n >= 1:
-    dp[0][1] = 1
-    dp[1][1] = 1
-    dp[2][1] = 1
+dp[0][1] = 1
+dp[1][1] = 1
+dp[2][1] = 1
 
-if n >= 2:
-    dp[0][2] = 3
-    dp[1][2] = 3
-    dp[2][2] = 3
+dp[0][2] = 3
+dp[1][2] = 3
+dp[2][2] = 3
 
-for i in range(3, n + 1):
+dp[0][3] = 9
+dp[1][3] = 9
+dp[2][3] = 8
+for i in range(4,n+1):
     # Good
-    dp[0][i] = (dp[0][i-1] + dp[1][i-1] + dp[2][i-1]) % MAX
-
+    dp[0][i] = dp[0][i-1] + dp[1][i-1] + dp[2][i-1]
     # Bad
-    dp[1][i] = (dp[0][i] - pow(2, i-3, MAX)) % MAX
-
+    dp[1][i] = dp[0][i] - 2 ** (i-3)
     # Te
-    if i >= 4:
-        dp[2][i] = (dp[0][i] - ((i-1)*(i-2) * pow(2, i-4, MAX))) % MAX
-    else:
-        dp[2][i] = dp[0][i] % MAX
+    dp[2][i] = dp[0][i] - ((i-1)*(i-2)* 2**(i-4))
 
-answer = (dp[0][n] + dp[1][n] + dp[2][n]) % MAX
-print(answer)
+answer = int(dp[0][n] + dp[1][n] + dp[2][n])
+print(answer%MAX)
