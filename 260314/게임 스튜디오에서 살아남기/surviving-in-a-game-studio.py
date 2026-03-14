e2851d88029522d32b51a8a52f4ce7c41491a821
Seungyeon Kim <sungyeon1227@gmail.com>
2026-03-14 18:34:36 +0900

    Title: 게임 스튜디오에서 살아남기, Time: 80ms, Memory: 19MB, Status: Runtime Error - Codetree

---
+++ 260314/게임 스튜디오에서 살아남기/surviving-in-a-game-studio.py
@@ -0,0 +1,24 @@
+n = int(input())
+MAX = 10**9 + 7
+
+# Please write your code here.
+dp = [[0] *(n+1) for _ in range(3)]
+
+dp[0][1] = 1
+dp[1][1] = 1
+dp[2][1] = 1
+
+dp[0][2] = 3
+dp[1][2] = 3
+dp[2][2] = 3
+
+for i in range(3,n+1):
+    # Good
+    dp[0][i] = dp[0][i-1] + dp[1][i-1] + dp[2][i-1]
+    # Bad
+    dp[1][i] = dp[0][i] - 2 ** (i-3)
+    # Te
+    dp[2][i] = dp[0][i] - ((i-1)*(i-2)* 2**(i-4))
+
+   
+print(int(dp[0][n] + dp[1][n] + dp[2][n])%MAX)
