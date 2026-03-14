5299de58ee1e81a846d2389fda55a0fbaa796050
Seungyeon Kim <sungyeon1227@gmail.com>
2026-03-14 21:16:02 +0900

    Title: 패턴 매칭, Time: 49ms, Memory: 16MB, Status: Passed - Codetree

--- 260314/패턴 매칭/pattern-matching.py
+++ 260314/패턴 매칭/pattern-matching.py
@@ -1,20 +1,30 @@
-S = input()
-P = input()
-flag =  'false'
-# Please write your code here.
-for p in range(len(P)):
-    if p >= len(S):
-        break
-    else:
-        if P[p] == '.':
-            flag = 'true'
-            continue
-        elif P[p] == '*':
-            break
-        
-        elif P[p] == S[p]:
-            flag = 'true'
-    
-print(flag)
-    
-        
+S = input().strip()
+P = input().strip()
+
+n = len(S)
+m = len(P)
+
+dp = [[False] * (m + 1) for _ in range(n + 1)]
+dp[0][0] = True
+
+# 빈 문자열과 패턴 매칭 처리
+for j in range(2, m + 1):
+    if P[j - 1] == '*':
+        dp[0][j] = dp[0][j - 2]
+
+for i in range(1, n + 1):
+    for j in range(1, m + 1):
+        # 일반 문자 또는 '.'
+        if P[j - 1] == '.' or P[j - 1] == S[i - 1]:
+            dp[i][j] = dp[i - 1][j - 1]
+
+        # '*'
+        elif P[j - 1] == '*':
+            # 앞 문자를 0번 사용하는 경우
+            dp[i][j] = dp[i][j - 2]
+
+            # 앞 문자를 1번 이상 사용하는 경우
+            if P[j - 2] == '.' or P[j - 2] == S[i - 1]:
+                dp[i][j] = dp[i][j] or dp[i - 1][j]
+
+print('true' if dp[n][m] else 'false')
