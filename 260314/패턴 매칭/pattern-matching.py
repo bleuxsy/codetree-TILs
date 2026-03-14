b73a7a83c63ac560d72a7599aa95ccdd4a66d589
Seungyeon Kim <sungyeon1227@gmail.com>
2026-03-14 21:09:49 +0900

    Title: 패턴 매칭, Time: 76ms, Memory: 19MB, Status: Runtime Error - Codetree

---
+++ 260314/패턴 매칭/pattern-matching.py
@@ -0,0 +1,17 @@
+S = input()
+P = input()
+flag =  'false'
+# Please write your code here.
+for p in range(len(P)):
+    if P[p] == '.':
+        flag = 'true'
+        continue
+    elif P[p] == '*':
+        break
+    
+    elif P[p] == S[p]:
+        flag = 'true'
+    
+print(flag)
+    
+        
