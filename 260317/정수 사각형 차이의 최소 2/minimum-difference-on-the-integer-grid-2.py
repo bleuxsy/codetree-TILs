dca5742d7db329a267efa8ef1cff7da087bfc68d
Seungyeon Kim <sungyeon1227@gmail.com>
2026-03-17 22:03:09 +0900

    Title: 정수 사각형 차이의 최소 2, Time: 122ms, Memory: 21MB, Status: Wrong Answer - Codetree

--- 260317/정수 사각형 차이의 최소 2/minimum-difference-on-the-integer-grid-2.py
+++ 260317/정수 사각형 차이의 최소 2/minimum-difference-on-the-integer-grid-2.py
@@ -3,32 +3,34 @@ def initialize():
         for j in range(n):
             dp[i][j] = INF
 
-    #1열 막행 초기화
+    # 1열 막행 초기화
     dp[0][0] = grid[0][0]
     for i in range(1, n):
-        dp[i][0] = max(dp[i-1][0], grid[i][0])
-    
+        dp[i][0] = max(dp[i - 1][0], grid[i][0])
+
     for j in range(1, n):
-        dp[0][j] = max(dp[0][j-1], grid[0][j])
+        dp[0][j] = max(dp[0][j - 1], grid[0][j])
+
 
 def solve(lower):
     for i in range(n):
         for j in range(n):
-            if grid[i][j] < lower :
+            if grid[i][j] < lower:
                 grid[i][j] = INF
     initialize()
 
     for i in range(1, n):
         for j in range(1, n):
-            dp[i][j] = max(min(dp[i-1][j], dp[i][j-1]), grid[i][j])
-    
-    return dp[n-1][n-1]
+            dp[i][j] = max(min(dp[i - 1][j], dp[i][j - 1]), grid[i][j])
+
+    return dp[n - 1][n - 1]
 
 
 n = int(input())
 grid = [list(map(int, input().split())) for _ in range(n)]
-INF =10 *8
-
+INF = 10 * 8
+ans = INF
+dp = [[0] * n for _ in range(n)]
 for lower in range(1, 101):
     upper = solve(lower)
 
