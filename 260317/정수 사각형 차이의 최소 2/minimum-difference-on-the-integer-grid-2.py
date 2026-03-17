45ee8015d75401b288772a8bf84c1208ff6d5082
Seungyeon Kim <sungyeon1227@gmail.com>
2026-03-17 21:44:08 +0900

    Title: 정수 사각형 차이의 최소 2, Time: 210ms, Memory: 23MB, Status: Passed - Codetree

--- 260317/정수 사각형 차이의 최소 2/minimum-difference-on-the-integer-grid-2.py
+++ 260317/정수 사각형 차이의 최소 2/minimum-difference-on-the-integer-grid-2.py
@@ -1,32 +1,40 @@
-def dfs(x, y, nmin, nmax):
-    global answer
-
-    if x == n - 1 and y == n - 1:
-        answer = min(answer, nmax - nmin)
-        return
-
-    for dx, dy in [(1, 0), (0, 1)]:
-        nx, ny = x + dx, y + dy
-
-        if 0 <= nx < n and 0 <= ny < n:
-            if not visited[nx][ny]:
-                new_min = min(nmin, grid[nx][ny])
-                new_max = max(nmax, grid[nx][ny])
-
-                visited[nx][ny] = 1
-                dfs(nx, ny, new_min, new_max)
-                visited[nx][ny] = 0
-
-
 n = int(input())
 grid = [list(map(int, input().split())) for _ in range(n)]
 
-INF = 10 ** 9
-answer = INF
+values = sorted(set(num for row in grid for num in row))
 
-visited = [[0] * n for _ in range(n)]
-visited[0][0] = 1
+def can(low, high):
+    if not (low <= grid[0][0] <= high):
+        return False
+    if not (low <= grid[n-1][n-1] <= high):
+        return False
 
-dfs(0, 0, grid[0][0], grid[0][0])
+    dp = [[False] * n for _ in range(n)]
+    dp[0][0] = True
+
+    for i in range(n):
+        for j in range(n):
+            if not (low <= grid[i][j] <= high):
+                continue
+
+            if i == 0 and j == 0:
+                continue
+
+            if i > 0 and dp[i-1][j]:
+                dp[i][j] = True
+            if j > 0 and dp[i][j-1]:
+                dp[i][j] = True
+
+    return dp[n-1][n-1]
+
+answer = 10**9
+
+for i in range(len(values)):
+    for j in range(i, len(values)):
+        low = values[i]
+        high = values[j]
+
+        if can(low, high):
+            answer = min(answer, high - low)
 
 print(answer)
