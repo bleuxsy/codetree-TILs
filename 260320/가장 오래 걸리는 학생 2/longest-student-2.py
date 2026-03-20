410ed1a0e4f049db25a857aa918d5b7a2cb601ea
Seungyeon Kim <sungyeon1227@gmail.com>
2026-03-20 18:16:14 +0900

    Title: 가장 오래 걸리는 학생 2, Time: 425ms, Memory: 39MB, Status: Passed - Codetree

--- 260320/가장 오래 걸리는 학생 2/longest-student-2.py
+++ 260320/가장 오래 걸리는 학생 2/longest-student-2.py
@@ -1,46 +1,29 @@
 import heapq
+
 n, m = map(int, input().split())
 edges = [tuple(map(int, input().split())) for _ in range(m)]
+
 INF = float('inf')
-# Please write your code here.
-D = [INF] * (n+1)
+D = [INF] * (n + 1)
 
-""" 
-    학교에서 각 지점으로 다익스트라 시켜야 함.
-    간선 방향을 반대로 바꿔야 함. 
-
-"""
-
-grid = [[INF] * (n+1) for _ in range(n+1)]
-
+# 간선 반대로 저장
+graph = [[] for _ in range(n + 1)]
+for a, b, d in edges:
+    graph[b].append((a, d))
 
 pq = []
-
-
-for edge in edges:
-    grid[edge[1]][edge[0]] = edge[2]
-
-grid[n][n] = 0
-visited = ([False] * (n+1))
 heapq.heappush(pq, (0, n))
 D[n] = 0
-D[0] = 0
+
 while pq:
     sw, idx = heapq.heappop(pq)
-    if visited[idx]:
+
+    if sw > D[idx]:
         continue
-    visited[idx] = True
 
-    for ny, nw in enumerate(grid[idx]):
-        if nw == INF :
-            continue
-
-        if  sw + nw < D[ny]:
-
-            D[ny] = sw +nw
-
+    for ny, nw in graph[idx]:
+        if sw + nw < D[ny]:
+            D[ny] = sw + nw
             heapq.heappush(pq, (D[ny], ny))
 
-
-
-print(max(D))
+print(max(D[1:n]))
