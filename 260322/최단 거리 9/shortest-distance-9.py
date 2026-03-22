c7a5c2f6f9f634cc070840b67243890dad828856
Seungyeon Kim <sungyeon1227@gmail.com>
2026-03-22 17:52:11 +0900

    Title: 최단 거리 9, Time: 49ms, Memory: 16MB, Status: Wrong Answer - Codetree

--- 260322/최단 거리 9/shortest-distance-9.py
+++ 260322/최단 거리 9/shortest-distance-9.py
@@ -1,45 +1,45 @@
 import heapq
-from collections import deque
+
 n, m = map(int, input().split())
 edges = [tuple(map(int, input().split())) for _ in range(m)]
 
-# Please write your code here.
 a, b = map(int, input().split())
 INF = 10**18
-path = [0] * (n+1)
-D = [INF]* (n+1)
+path = [0] * (n + 1)
+D = [INF] * (n + 1)
 pq = []
-graph = [[] for _ in range(n+1)]
+graph = [[] for _ in range(n + 1)]
 
-for edge in edges:
-    w = edge[2]
-    x = edge[0]
-    y = edge[1]
+for x, y, w in edges:
     graph[x].append((y, w))
 
 D[a] = 0
 heapq.heappush(pq, (0, a))
-visited = [False] *(n+1)
+visited = [False] * (n + 1)
+
 while pq:
-    cw , cx = heapq.heappop(pq)
+    cw, cx = heapq.heappop(pq)
 
     if visited[cx]:
         continue
     visited[cx] = True
-    for nx , nw in graph[cx]:
+
+    for nx, nw in graph[cx]:
         if D[nx] > D[cx] + nw:
             D[nx] = D[cx] + nw
             path[nx] = cx
             heapq.heappush(pq, (D[nx], nx))
 
+if D[b] == INF:
+    print(-1)
+else:
+    answer = []
+    x = b
+    while x != 0:
+        answer.append(x)
+        if x == a:
+            break
+        x = path[x]
 
-answer = []
-x = b
-answer.append(x)
-while x != a:
-    x = path[x]
-    answer.append(x)
-
-print(D[b])
-for a in answer[::-1]:
-    print(a , end=" ")
+    print(D[b])
+    print(*answer[::-1])
