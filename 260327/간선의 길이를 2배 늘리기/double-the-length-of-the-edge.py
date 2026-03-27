86549c2153c3a8672a7f2d9e1fac9b230b512cb1
Seungyeon Kim <sungyeon1227@gmail.com>
2026-03-27 16:46:12 +0900

    Title: 간선의 길이를 2배 늘리기, Time: 0ms, Memory: 0MB, Status: Time Limit Exceed - Codetree

--- 260327/간선의 길이를 2배 늘리기/double-the-length-of-the-edge.py
+++ 260327/간선의 길이를 2배 늘리기/double-the-length-of-the-edge.py
@@ -1,72 +1,50 @@
-
-# Please write your code here.
-"""
-
-양방향
-한 개의 간선을 골라 길이를 2배늘려서 1 - N까지 도달하는 데의 최단거리를 최대
-2배 늘리기전과 후의 차이 출력
-
-
-
-
-5 7
-2 1 5
-1 3 1
-3 2 8
-3 5 7
-3 4 3
-2 4 7
-4 5 2
-
-
-"""
 import heapq
+
+INF = 10**18
+
 def dijkstra(graph):
-    visited = [False] * (n+1)
-    B = [INF] * (n+1)
+    dist = [INF] * (n + 1)
     pq = []
-    heapq.heappush(pq, (0 , 1))
-    # 시작점은 1
-    B[1] = 0
+    dist[1] = 0
+    heapq.heappush(pq, (0, 1))
 
     while pq:
-        w, sx = heapq.heappop(pq)
+        cur_dist, x = heapq.heappop(pq)
 
-        if visited[sx] :
+        if dist[x] < cur_dist:
             continue
-        visited[sx] = True
 
-        for nx, nw in graph[sx]:
-            if B[nx] > B[sx] + nw:
-                B[nx] = B[sx] + nw
-                heapq.heappush(pq, (B[nx], nx))
+        for nx, w in graph[x]:
+            nd = cur_dist + w
+            if dist[nx] > nd:
+                dist[nx] = nd
+                heapq.heappush(pq, (nd, nx))
 
+    return dist[n]
 
-
-    return B[n]
 n, m = map(int, input().split())
 edges = [tuple(map(int, input().split())) for _ in range(m)]
 
+grid = [[] for _ in range(n + 1)]
+for a, b, w in edges:
+    grid[a].append((b, w))
+    grid[b].append((a, w))
 
-INF = 10**18
-grid = [[] for _ in range(n+1)]
-for x, y, w in edges:
-    grid[x].append((y, w))
-    grid[y].append((x, w))
-
-
-answer = 0
 org = dijkstra(grid)
-for i in range(1, n+1):
-    new_grid = [[] for _ in range(n+1)]
+
+answer = org
+
+for i in range(m):   # 간선 전체를 다 봐야 함
+    new_grid = [[] for _ in range(n + 1)]
+
     for j in range(m):
         a, b, w = edges[j]
         if j == i:
-            w*= 2
-        new_grid[a].append((b,w))
-        new_grid[b].append((a,w))
+            w *= 2
+        new_grid[a].append((b, w))
+        new_grid[b].append((a, w))
 
-    new = dijkstra(new_grid)
-    answer = max(answer, new)
+    new_dist = dijkstra(new_grid)
+    answer = max(answer, new_dist)
 
 print(answer - org)
